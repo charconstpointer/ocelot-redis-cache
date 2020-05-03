dca022600b6e33f7a68ecbfb87ca8d8978d353a6
mo <attwistingnether@gmail.com>
2020-05-03 21:09:26 +0200

    Ocelot.Redis

--- Missy.Api/Startup.cs
+++ Missy.Api/Startup.cs
@@ -25,22 +25,12 @@ namespace Missy.Api
         {
             services.AddOcelot()
                 .AddRedisCache("localhost", "eksdee");
-            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            // app.UseHttpsRedirection();
             app.UseOcelot();
-            app.UseRouting();
-
-            app.UseAuthorization();
         }
     }
 }
